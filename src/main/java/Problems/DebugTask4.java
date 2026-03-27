e8636590de3edc797e1b8ad8878919b134fa92c6
Megobitik <118119998+Megobitik@users.noreply.github.com>
2026-03-27 16:32:56 +0300

    fix debugtask4

--- src/main/java/Problems/DebugTask4.java
+++ src/main/java/Problems/DebugTask4.java
@@ -2,10 +2,15 @@ package Problems;
 
 public class DebugTask4 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("non"));
+        System.out.println(isPalindrome(null));
     }
     public static boolean isPalindrome(String str) {
-        String reversed = new StringBuilder(str).reverse().toString();
-        return str.equals(reversed);
+        if (str == null){
+            return false;
+        }
+        else {
+            String reversed = new StringBuilder(str).reverse().toString();
+            return str.equals(reversed);
+        }
     }
 }
